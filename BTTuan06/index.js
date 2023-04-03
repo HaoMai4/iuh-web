1234939efb05294b363bb1f29924c2d4f9d16944
HaoMai4 <121676028+HaoMai4@users.noreply.github.com>
2023-04-03 17:13:14 +0700

    Add files via upload

---
+++ BTTuan06/index.js
@@ -0,0 +1,34 @@
+var start = document.querySelector(".btn-start")
+var stop = document.querySelector(".btn-stop")
+var reset = document.querySelector(".btn-reset")
+
+var minute = document.querySelector(".minute")
+var second = document.querySelector(".second")
+
+var interval
+
+function time() {
+    var giay = parseInt(second.textContent)
+    var phut = parseInt(minute.textContent)
+
+    giay++;
+
+    if (giay == 59) {
+        phut++;
+        giay = 0;
+    }
+    if (giay < 10) {
+        second.textContent = "0" + giay
+    } else
+        second.textContent = giay
+
+    if (phut < 10) {
+        minute.textContent = "0" + phut
+    } else
+        minute.textContent = phut
+};
+start.addEventListener('click', function() {
+    if (interval == null) {
+        interval = setInterval(time, 1000)
+    }
+})
